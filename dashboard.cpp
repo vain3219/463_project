b058133add652540663912da9f70d92f6ae99b60
Cody Thompson <cody5887@yahoo.com>
2021-04-08 01:59:27 -0700

    Added room status buttons

--- dashboard.cpp
+++ dashboard.cpp
@@ -19,13 +19,18 @@ DashBoard::DashBoard(QWidget *parent)
     palette.setColor(ui->DatabaseLoadError->foregroundRole(), Qt::red);
     ui->DatabaseLoadError->setPalette(palette);
     //
+    // Set initial GUI enviornment
+    ui->stackedWidgetRDB->setCurrentIndex(1);
+    setBlankPage();
+    //
 
     // Establish connection with SQLite Database
     DbManager db("Antares Management Database.db");
 
     // Attempt to populate the DataTable
     //  If the DataTable can't be populated, display error text
-    if( !PopulateDataTable(db) ) {
+    if( !PopulateDataTable(db) )
+    {
         ui->DataTable->hide();
         ui->DatabaseLoadError->show();
     }
@@ -47,7 +52,7 @@ void DashBoard::on_LogoutButton_clicked()
 void DashBoard::on_SearchButton_clicked()
 {
     // Set the stacked widget to display the additional search GUI
-    ui->stackedWidget->setCurrentWidget(ui->SearchPage);  
+    ui->stackedWidgetSR->setCurrentWidget(ui->SearchPage);
 }
 
 void DashBoard::on_DailyButton_clicked()
@@ -76,14 +81,19 @@ void DashBoard::on_InfoButton_clicked()
 
 void DashBoard::on_ReservationButton_clicked()
 {
+    // Hide unecessary GUI elements
+    setBlankPage();
     // Set the stacked widget to display the additional reservation GUI
-    ui->stackedWidget->setCurrentWidget(ui->ReservationPage);
+    ui->stackedWidgetSR->setCurrentWidget(ui->ReservationPage);
 }
 
 void DashBoard::on_RoomsButton_clicked()
 {
     // Hide unecessary GUI elements
     setBlankPage();
+    // Set the stacked widget to display the room buttons
+    setRoomStatusButtons();
+    ui->stackedWidgetRDB->setCurrentIndex(0);
 }
 
 void DashBoard::on_WeeklyButton_clicked()
@@ -116,8 +126,199 @@ bool DashBoard::PopulateDataTable(DbManager db)
 void DashBoard::setBlankPage() {
     // Sets the stacked widget to display the blankPage when additional GUI
     //  elements are unecessary
-    if(ui->stackedWidget->currentIndex() != 0) {
-            ui->stackedWidget->setCurrentWidget(ui->BlankPage);
+    if(ui->stackedWidgetSR->currentIndex() != 0)
+    {
+        ui->stackedWidgetSR->setCurrentWidget(ui->BlankPage);
+    }
+    if(ui->stackedWidgetRDB->currentIndex() == 0)
+    {
+        ui->stackedWidgetRDB->setCurrentIndex(1);
     }
 }
+
+void DashBoard::setRoomButton(int roomNumber, QPushButton* button)
+// void DashBoard::setRoomButton(int roomNumber, QPushButton* button, roomStatus status)
+/*
+ * We can use a database query to retrieve the value for the room status and append it to the strin as
+ * I have commented out below.  This would allow dynamic button text and color.
+ */
+{
+    QString* buttonText = new QString("Room " + QString::number(roomNumber) + " - #STATUS HERE#");// + getStringStatus(Query));
+    button->setText(*buttonText);
+    // Random coloring for testing
+    int statusTemp = rand() % 4 + 0;
+    //
+    setColor(roomStatus(statusTemp), button);
+}
+
+QString DashBoard::getStatusString(roomStatus status)
+{
+    switch(status)
+    {
+        case AVAILABLE:
+            return "Available";
+            break;
+
+        case OCCUPIED:
+            return "Occupied";
+            break;
+
+        case DIRTY:
+            return "Dirty";
+            break;
+
+        case MAINTENANCE:
+            return "Maintenance Needed";
+            break;
+
+        default:
+            return "Contact Tech Support";
+            break;
+    }
+}
+
+void DashBoard::setColor(roomStatus status, QPushButton* button) {
+    switch(status)
+    {
+        case AVAILABLE:
+            // Set Green
+            button->setStyleSheet("font: bold;background-color : green;");
+            break;
+
+        case OCCUPIED:
+            // Set Red
+            button->setStyleSheet("font: bold;background-color : red");
+            break;
+
+        case DIRTY:
+            // Set Yellow
+            button->setStyleSheet("font: bold;background-color : yellow;");
+            break;
+
+        case MAINTENANCE:
+            // Set Orange
+            button->setStyleSheet("font: bold;background-color : orange;");
+            break;
+
+        default:
+            // No Change -- defualt Grey
+            break;
+    }
+}
+
+void DashBoard::setRoomStatusButtons()
+{
+        srand (time(NULL));
+        setRoomButton(10, ui->RoomButton_10);
+        setRoomButton(11, ui->RoomButton_11);
+        setRoomButton(12, ui->RoomButton_12);
+        setRoomButton(13, ui->RoomButton_13);
+        setRoomButton(14, ui->RoomButton_14);
+        setRoomButton(15, ui->RoomButton_15);
+        setRoomButton(16, ui->RoomButton_16);
+        setRoomButton(17, ui->RoomButton_17);
+        setRoomButton(18, ui->RoomButton_18);
+        setRoomButton(19, ui->RoomButton_19);
+        setRoomButton(20, ui->RoomButton_20);
+        setRoomButton(21, ui->RoomButton_21);
+        setRoomButton(22, ui->RoomButton_22);
+        setRoomButton(23, ui->RoomButton_23);
+        setRoomButton(24, ui->RoomButton_24);
+}
+
+void DashBoard::setRoomDetails(int roomNumber, QPushButton* button)
+{
+    QString details =
+    "<body><style>#boxes {content: "";display: table;clear: both;}"
+    "div {float: left;height: 470px;width: 23%;padding: 0 10px;}</style>"
+    "<main id=\"boxes\">"
+    ""
+    "<div><h1>Room " + QString::number(roomNumber)+ " - #ROOM STATUS HERE# </h1>\n"
+    "<p>This is information relevent to the rooms status.</p>"
+    "<p>All of this text will be presented on the screen.</p></div>"
+    "<p>That's right; look no further! More text!</p>"
+    "<p>We can have more text here also! Yay html!</p>"
+    "</div></main></body>";
+
+    /*
+     * Some SQL Query logic here and create a more detialed report
+     */
+    ui->textEdit->setText(details);
+}
+
+void DashBoard::on_RoomButton_10_clicked()
+{
+    setRoomDetails(10, ui->RoomButton_10);
+}
+
+void DashBoard::on_RoomButton_11_clicked()
+{
+    setRoomDetails(11, ui->RoomButton_11);
+}
+
+void DashBoard::on_RoomButton_12_clicked()
+{
+    setRoomDetails(12, ui->RoomButton_12);
+}
+
+void DashBoard::on_RoomButton_13_clicked()
+{
+    setRoomDetails(13, ui->RoomButton_13);
+}
+
+void DashBoard::on_RoomButton_14_clicked()
+{
+    setRoomDetails(14, ui->RoomButton_14);
+}
+
+void DashBoard::on_RoomButton_15_clicked()
+{
+    setRoomDetails(15, ui->RoomButton_15);
+}
+
+void DashBoard::on_RoomButton_16_clicked()
+{
+    setRoomDetails(16, ui->RoomButton_16);
+}
+
+void DashBoard::on_RoomButton_17_clicked()
+{
+    setRoomDetails(17, ui->RoomButton_17);
+}
+
+void DashBoard::on_RoomButton_18_clicked()
+{
+    setRoomDetails(18, ui->RoomButton_18);
+}
+
+void DashBoard::on_RoomButton_19_clicked()
+{
+    setRoomDetails(19, ui->RoomButton_19);
+}
+
+void DashBoard::on_RoomButton_20_clicked()
+{
+    setRoomDetails(20, ui->RoomButton_20);
+}
+
+void DashBoard::on_RoomButton_21_clicked()
+{
+    setRoomDetails(21, ui->RoomButton_21);
+}
+
+void DashBoard::on_RoomButton_22_clicked()
+{
+    setRoomDetails(22, ui->RoomButton_22);
+}
+
+void DashBoard::on_RoomButton_23_clicked()
+{
+    setRoomDetails(23, ui->RoomButton_23);
+}
+
+void DashBoard::on_RoomButton_24_clicked()
+{
+    setRoomDetails(24, ui->RoomButton_24);
+}
+
 
--- dashboard.h
+++ dashboard.h
@@ -4,10 +4,18 @@
 #include <QMainWindow>
 #include "DbManager.h"
 #include "reservations.h"
+#include <cmath>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class DashBoard; }
 QT_END_NAMESPACE
+
+enum roomStatus {
+    AVAILABLE,
+    OCCUPIED,
+    DIRTY,
+    MAINTENANCE
+};
 
 class DashBoard : public QMainWindow
 {
@@ -40,6 +48,36 @@ private slots:
 
     void on_MakeResButton_clicked();
 
+    void on_RoomButton_10_clicked();
+
+    void on_RoomButton_11_clicked();
+
+    void on_RoomButton_12_clicked();
+
+    void on_RoomButton_13_clicked();
+
+    void on_RoomButton_14_clicked();
+
+    void on_RoomButton_15_clicked();
+
+    void on_RoomButton_16_clicked();
+
+    void on_RoomButton_17_clicked();
+
+    void on_RoomButton_18_clicked();
+
+    void on_RoomButton_19_clicked();
+
+    void on_RoomButton_20_clicked();
+
+    void on_RoomButton_21_clicked();
+
+    void on_RoomButton_22_clicked();
+
+    void on_RoomButton_23_clicked();
+
+    void on_RoomButton_24_clicked();
+
 private:
     Ui::DashBoard *ui;
 
@@ -51,5 +89,23 @@ private:
     //  except for the Reservations button and the search button as these
     //  requirements need additional GUI elements
     void setBlankPage();
+
+    // Used to set the text and color of the QPushButtons for the Room Status display
+    void setRoomButton(int roomNumber, QPushButton* button);
+    void setRoomButton(int roomNumber, QPushButton* button, roomStatus status);
+
+    // Returns the respective string for the given enum roomStatus value defined
+    //  before the DashBoard class
+    QString getStatusString(roomStatus status);
+
+    // Sets the color of room status buttons
+    void setColor(roomStatus status, QPushButton* button);
+
+    // Sets all room status button text and colors
+    void setRoomStatusButtons();
+
+    // Sets the text of the Text Edit field for the respective room button.  This displays
+    //  detailed information on the rooms status.
+    void setRoomDetails(int roomNumber, QPushButton* button);
 };
 #endif // DASHBOARD_H
